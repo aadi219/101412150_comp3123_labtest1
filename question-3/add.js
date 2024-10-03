1d35ba5c2010ef59576d67ac9ad6405da3ec0b7b
Aadi <aaadibadola@gmail.com>
2024-10-03 17:49:32 -0400

    question - 3 fix after clarification

--- question-3/add.js
+++ question-3/add.js
@@ -1,23 +1,22 @@
 const fs = require("fs");
 const path = require("path");
-const process = require("process");
 
 const LOG_PATH = path.join(__dirname, "Logs");
 
 try {
   fs.mkdirSync(LOG_PATH);
-  process.chdir(LOG_PATH);
   addFiles();
 } catch (err) {
+  console.error("Error while creating logs!");
   console.log(err);
 }
 
 function addFiles() {
   for (let i = 0; i < 10; i++) {
     const fileName = `logs${i}.txt`;
+    const filePath = path.join(LOG_PATH, fileName);
     const data = `Some data in ${fileName}`;
-    fs.writeFileSync(fileName, data);
+    fs.writeFileSync(filePath, data);
     console.log(`${fileName} created`);
   }
 }
-addFiles();
